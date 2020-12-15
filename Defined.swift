ec4853e4ceb513eb26a3f5dc4023b0b98e509eaf
Lingxi_Li <36816148+lilingxi01@users.noreply.github.com>
2020-12-14 23:21:33 -0500

    Create Defined.swift

---
+++ Defined.swift
@@ -0,0 +1,16 @@
+//
+//  Defined.swift
+//
+//  Created by Lingxi Li on 12/14/20.
+//
+
+import Foundation
+import SwiftUI
+
+class Defined {
+    /// 版本号
+    static let version: String = "0.1"
+    
+    /// 版本号码
+    static let versionNumber: Int = 1
+}
