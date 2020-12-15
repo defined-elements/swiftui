07c3dc3dc684df01e3ea958615e3c67263622b6e
Lingxi_Li <36816148+lilingxi01@users.noreply.github.com>
2020-12-14 23:21:57 -0500

    Create DefinedDimens.swift

---
+++ DefinedDimens.swift
@@ -0,0 +1,15 @@
+//
+//  DefinedDimens.swift
+//
+//  Created by Lingxi Li on 12/14/20.
+//
+
+import Foundation
+import SwiftUI
+
+extension Defined {
+    class Dimens {
+        /// 圆角 - 标准
+        static let CORNER_STR: CGFloat = 30
+    }
+}
