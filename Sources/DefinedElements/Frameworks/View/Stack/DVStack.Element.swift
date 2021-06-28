69dc1341e3e41d3c35cfc20ed10d26e04b694e50
Lingxi_Li <36816148+lilingxi01@users.noreply.github.com>
2021-06-28 12:26:27 -0400

    [DVStack] Improve the constrains and identification on `DefinedViewStackElement`.

--- Sources/DefinedElements/Frameworks/View/Stack/DVStack.Element.swift
+++ Sources/DefinedElements/Frameworks/View/Stack/DVStack.Element.swift
@@ -1,13 +1,20 @@
+#if os(iOS)
+
 import Foundation
 import SwiftUI
 
 /// 该结构服务于`DefinedViewStack` < 内部组件 >
+///
+/// - Note: The reason why we do not use generic type for `DefinedPage` is we will not be able to define an array of this.
 struct DefinedViewStackElement : Identifiable, Equatable {
-    ///
-    static var constantId: Int = 0
+    /// [Internal]
+    static var constantLevel: Int = 0
     
     /// Element 标签
-    let id: String
+    let id: UUID
+    
+    ///
+    let level: Int
     
     /// Element 内容
     let content: AnyView
@@ -17,36 +24,16 @@ struct DefinedViewStackElement : Identifiable, Equatable {
     /// - Note: 状态栏设定的变动在ViewManager中而非此处，该值仅用于页面初始化的时候
     var statusBarStyle: UIStatusBarStyle = .darkContent
     
-    /// 构造器 - AnyView
-    ///
-    /// - Parameter view: 给定视图
-    init(_ view: AnyView) {
-        self.id = "constant-\(DefinedPageElement.constantId)"
-        self.content = view
-        
-        DefinedPageElement.constantId += 1
-    }
-    
     /// 构造器 - DefinedPage
     ///
     /// - Parameter view: 给定视图
-    init<Page>(_ view: Page) where Page: DefinedPage {
-        self.id = "constant-\(DefinedPageElement.constantId)"
-        self.content = AnyView(view)
-        self.statusBarStyle = view.statusBarStyle
+    init<Page>(_ page: Page) where Page: DefinedPage {
+        self.id = page.id
+        self.level = DefinedViewStackElement.constantLevel
+        self.content = AnyView(page)
+        self.statusBarStyle = page.statusBarStyle
         
-        DefinedPageElement.constantId += 1
-    }
-    
-    /// 构造器 - DefinedPage + Tag
-    ///
-    /// - Parameters:
-    ///   - tag: 页面标签（用于只出现一次的页面，防止页面被先后推送至不同层次或反复使用）
-    ///   - view: 给定视图
-    init<Page>(_ tag: String, _ view: Page) where Page: DefinedPage {
-        self.id = tag
-        self.content = AnyView(view)
-        self.statusBarStyle = view.statusBarStyle
+        DefinedViewStackElement.constantLevel += 1
     }
 
     /// 用于相同比较 < 内部函数 >
@@ -55,3 +42,4 @@ struct DefinedViewStackElement : Identifiable, Equatable {
     }
 }
 
+#endif
