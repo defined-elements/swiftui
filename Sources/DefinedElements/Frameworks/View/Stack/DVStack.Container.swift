e47557088468b0a10f5c57732f2a2b9486d33e37
Lingxi_Li <36816148+lilingxi01@users.noreply.github.com>
2021-07-11 13:52:25 -0400

    [DVStack] Documented the `DVStackContainer`.

--- Sources/DefinedElements/Frameworks/View/Stack/DVStack.Container.swift
+++ Sources/DefinedElements/Frameworks/View/Stack/DVStack.Container.swift
@@ -2,35 +2,42 @@
 
 import Foundation
 
-/// [DE Internal]
-struct DefinedViewStackContainer {
-    ///
+/// [DE Internal] A view stack container instance holding page elements.
+internal struct DefinedViewStackContainer {
+    /// The stack instance holding page elements.
     var stack = [DefinedViewStackElement]()
     
+    /// The count of elements in this container.
     var count: Int {
         return stack.count
     }
     
+    /// [DE Internal] Get the stack directly.
     ///
-    func getStack() -> [DefinedViewStackElement] {
+    /// - Note: For `elements` property updating purpose only!
+    internal func getStack() -> [DefinedViewStackElement] {
         return stack
     }
     
-    ///
+    /// [DE Internal] Push a page element onto the stack.
     mutating func push(_ target: DefinedViewStackElement) {
         guard stack.firstIndex(of: target) == nil else {
-            // 不同层级间重复出现的Page
+            // if there exists a page element having same id with target page, it means that something went wrong.
+            // we should never push the same page twice (we can have different instances of the same page, but no exactly same page instance).
             return
         }
         stack.append(target)
     }
     
+    /// [DE Internal] Pop a page element onto the stack.
     ///
+    /// We do NOT return the element since it becomes totally useless once it has been popped.
+    /// There makes no sense to get it back outside.
     mutating func pop() {
         _ = stack.popLast()
     }
     
-    ///
+    /// [DE Internal] Clean the entire container.
     mutating func removeAll() {
         stack.removeAll()
     }
