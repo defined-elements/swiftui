f43fb804c8200edb398a8999a467526842324bc1
Lingxi Li <lilingxi01@gmail.com>
2021-07-19 12:07:54 -0400

    [DV] Add some todos and fix the unexpected setter in extension.

--- Sources/DefinedElements/Frameworks/View/DV.Protocol.swift
+++ Sources/DefinedElements/Frameworks/View/DV.Protocol.swift
@@ -1,4 +1,3 @@
-import Foundation
 import SwiftUI
 
 // MARK: - Protocol
@@ -7,13 +6,15 @@ import SwiftUI
 ///
 /// You can also use it if you want to create your own `DefinedView` component.
 ///
-/// - TODO: Fix `status` pointer problem.
+/// - TODO: Finish the development of Configurations.
 public protocol DefinedView : View {
     /// Configurate all things related to view frame.
     ///
     /// If you do not want to constrain the view with a bridge frame, you can disable it:
     ///
-    ///     var viewConfiguration = DefinedViewConfiguration(inactive: true)
+    /// ``` swift
+    /// var viewConfiguration = DefinedViewConfiguration(inactive: true)
+    /// ```
     ///
     /// otherwise, you can set the default frame size by using basic initializer
     /// `init(default:loading:active:done:error:disabled:)`.
@@ -25,7 +26,9 @@ public protocol DefinedView : View {
     ///
     /// If you do not want to constrain the view with background views, you can disable it:
     ///
-    ///     var backgroundConfiguration = DefinedViewBackgroundConfiguration(inactive: true)
+    /// ``` swift
+    /// var backgroundConfiguration = DefinedViewBackgroundConfiguration(inactive: true)
+    /// ```
     ///
     /// otherwise, you can set the default background views by using basic initializer
     /// `init(default:loading:active:done:error:disabled:)`.
@@ -36,8 +39,9 @@ public protocol DefinedView : View {
     /// Configurate all things related to border setup.
     ///
     /// If you do not want to constrain the view with a border, you can disable it:
-    ///
-    ///     var borderConfiguration = DefinedViewBorderConfiguration(inactive: true)
+    /// ``` swift
+    /// var borderConfiguration = DefinedViewBorderConfiguration(inactive: true)
+    /// ```
     ///
     /// otherwise, you can set the border setup by using basic initializer
     /// `init(default:loading:active:done:error:disabled:)`.
@@ -48,8 +52,9 @@ public protocol DefinedView : View {
     /// Configurate all things related to overlay view.
     ///
     /// If you do not want to constrain the view with an overlay, you can disable it:
-    ///
-    ///     var overlayConfiguration = DefinedViewOverlayConfiguration(inactive: true)
+    /// ``` swift
+    /// var overlayConfiguration = DefinedViewOverlayConfiguration(inactive: true)
+    /// ```
     ///
     /// otherwise, you can set the default overlay views by using basic initializer
     /// `init(default:loading:active:done:error:disabled:)`.
@@ -57,11 +62,8 @@ public protocol DefinedView : View {
     /// - Note: This is only a configuration for the root view, not for the hierarchy level.
     var overlayConfiguration: DefinedViewOverlayConfiguration { get set }
     
-    ///
+    /// The status of an UI component.
     var status: GeneralStatus { get set }
-    
-    // TODO: It seems like having some problem on using `$` prefix on Swift 5.5
-    // var $status: Binding<GeneralStatus> { get set }
 }
 
 // MARK: - Extensions for fields
@@ -72,8 +74,9 @@ extension DefinedView {
         get {
             DefinedViewConfiguration(inactive: true)
         }
-        set(value) {
-            self.viewConfiguration = value
+        set {
+            // WARNING: should not be assigned without defining it locally!
+            DefinedWarning.send(from: "DefinedViewProtocol", "You should NOT do things with viewConfiguration if you did not define it locally. Nothing is going to be changed if you proceed.")
         }
     }
     
@@ -82,8 +85,9 @@ extension DefinedView {
         get {
             DefinedViewBackgroundConfiguration(inactive: true)
         }
-        set(value) {
-            self.backgroundConfiguration = value
+        set {
+            // WARNING: should not be assigned without defining it locally!
+            DefinedWarning.send(from: "DefinedViewProtocol", "You should NOT do things with backgroundConfiguration if you did not define it locally. Nothing is going to be changed if you proceed.")
         }
     }
     
@@ -92,8 +96,9 @@ extension DefinedView {
         get {
             DefinedViewBorderConfiguration(inactive: true)
         }
-        set(value) {
-            self.borderConfiguration = value
+        set {
+            // WARNING: should not be assigned without defining it locally!
+            DefinedWarning.send(from: "DefinedViewProtocol", "You should NOT do things with borderConfiguration if you did not define it locally. Nothing is going to be changed if you proceed.")
         }
     }
     
@@ -102,8 +107,9 @@ extension DefinedView {
         get {
             DefinedViewOverlayConfiguration(inactive: true)
         }
-        set(value) {
-            self.overlayConfiguration = value
+        set {
+            // WARNING: should not be assigned without defining it locally!
+            DefinedWarning.send(from: "DefinedViewProtocol", "You should NOT do things with overlayConfiguration if you did not define it locally. Nothing is going to be changed if you proceed.")
         }
     }
     
@@ -112,8 +118,9 @@ extension DefinedView {
         get {
             return .default
         }
-        set(value) {
-            self.status = value
+        set {
+            // WARNING: should not be assigned without defining it locally!
+            DefinedWarning.send(from: "DefinedViewProtocol", "You should NOT do things with status if you did not define it locally. Nothing is going to be changed if you proceed.")
         }
     }
 }
