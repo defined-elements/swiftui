203a04f8c465b8e50bf6446ec03b9bf1db11b652
Lingxi Li <lilingxi01@gmail.com>
2021-12-20 22:54:44 -0500

    [DTextPreset] Add font weight variation support.

--- Sources/DefinedElements/Text/Presets/Support/FontWeight+.FontWeight.swift
+++ Sources/DefinedElements/Text/Presets/Support/FontWeight+.FontWeight.swift
@@ -1,8 +1,55 @@
-//
-//  File.swift
-//  
-//
-//  Created by Lingxi Li on 12/20/21.
-//
+import SwiftUI
 
-import Foundation
+extension Font.Weight {
+    /// Get a smaller font weight based on given font weight.
+    static func minus(_ origin: Font.Weight) -> Font.Weight {
+        switch origin {
+        case .black:
+            return .heavy
+        case .heavy:
+            return .bold
+        case .bold:
+            return .semibold
+        case .semibold:
+            return .medium
+        case .medium:
+            return .regular
+        case .regular:
+            return .thin
+        case .thin:
+            return .light
+        case .light:
+            return .ultraLight
+        case .ultraLight:
+            return .ultraLight
+        default:
+            return .regular
+        }
+    }
+    
+    /// Get a larger font weight based on given font weight.
+    static func plus(_ origin: Font.Weight) -> Font.Weight {
+        switch origin {
+        case .black:
+            return .black
+        case .heavy:
+            return .black
+        case .bold:
+            return .heavy
+        case .semibold:
+            return .bold
+        case .medium:
+            return .semibold
+        case .regular:
+            return .medium
+        case .thin:
+            return .regular
+        case .light:
+            return .thin
+        case .ultraLight:
+            return .light
+        default:
+            return .regular
+        }
+    }
+}
