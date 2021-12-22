166dec5bb56fe9f2a8dd62593b150a825e3a0b98
Lingxi Li <lilingxi01@gmail.com>
2021-12-21 23:38:20 -0500

    [DPreset] Add DPreset Protocol.

---
+++ Sources/DefinedElements/Frameworks/View/Preset/DPreset.Protocol.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+/// [DE] Global preset protocol.
+///
+/// Used for modules to support global preset setting function.
+public protocol DefinedPresetProtocol {
+    // No need for anything.
+    // We are just going to cast things when handling them.
+}
