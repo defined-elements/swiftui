9d9602df32f114edddcbf2aac45ecbed1b45bca6
Lingxi Li <lilingxi01@gmail.com>
2021-12-28 13:42:25 -0500

    [DBlur] Simply format the code.

--- Sources/DefinedElements/Blur/DBlur.Background.swift
+++ Sources/DefinedElements/Blur/DBlur.Background.swift
@@ -5,6 +5,8 @@ public struct DefinedBlurBackground : DefinedView {
     let opa: Double = DEColor.std.opa
     
     public var body: some View {
-        DefinedBlur(blurStyle: .systemUltraThinMaterial).overlay(DEColor.blur.light)
+        DefinedBlur(
+            blurStyle: .systemUltraThinMaterial
+        ).overlay(DEColor.blur.light)
     }
 }
