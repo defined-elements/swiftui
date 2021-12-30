64511137e4f4afbac27131a41540e2dbd02f84dc
Lingxi Li <lilingxi01@gmail.com>
2021-12-29 23:55:08 -0500

    [DCover] Add pred version of Cover.
    This is the predecessor version of Cover System! It has a lot of bug and does not follow the design pattern at all! It is for temporary test and 100% NOT for production or even formal developing! Please proceed with extra care!

---
+++ Sources/DefinedElements/CoverSystem/CoverHeader/Support/DCHeader.#Style.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lingxi Li on 4/20/21.
+//
+
+import Foundation
+import SwiftUI
+
+///
+public enum CoverHeaderStyle {
+    ///
+    case center
+    
+    ///
+    case center_side
+    
+    ///
+    case magazine
+}
